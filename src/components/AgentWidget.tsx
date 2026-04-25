70c8b193e59bd74cf480bd592cbba6b5b54dfed7
Debrani <reddydebrani@gmail.com>
2026-04-25 11:18:18 +0530

    fix embedded messaging tsx integration

--- src/components/AgentWidget.tsx
+++ src/components/AgentWidget.tsx
@@ -2,49 +2,38 @@ import { useEffect } from "react";
 
 declare global {
   interface Window {
-    embeddedservice_bootstrap: any;
+    embeddedservice_bootstrap?: any;
   }
 }
 
 export default function AgentWidget() {
   useEffect(() => {
-    function initEmbeddedMessaging() {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://orgfarm-ec702c7fe6-dev-ed.develop.my.site.com/ESWPortfolioWebsiteChat1777095730674/assets/js/bootstrap.min.js";
+
+    script.async = true;
+
+    script.onload = () => {
       try {
         window.embeddedservice_bootstrap.settings.language = "en_US";
 
         window.embeddedservice_bootstrap.init(
           "00DgL00000OLNLZ",
-          "Portfolio_Web_Chat",
-          "https://orgfarm-ec702c7fe6-dev-ed.develop.my.site.com/ESWPortfolioWebChat1777093773265",
+          "Portfolio_Website_Chat",
+          "https://orgfarm-ec702c7fe6-dev-ed.develop.my.site.com/ESWPortfolioWebsiteChat1777095730674",
           {
             scrt2URL:
               "https://orgfarm-ec702c7fe6-dev-ed.my.salesforce-scrt.com"
           }
         );
       } catch (err) {
-        console.error("Error loading Embedded Messaging:", err);
+        console.error("Embedded Messaging error:", err);
       }
-    }
+    };
 
-    const existing = document.querySelector(
-      'script[data-salesforce="true"]'
-    );
-
-    if (!existing) {
-      const script = document.createElement("script");
-
-      script.src =
-        "https://orgfarm-ec702c7fe6-dev-ed.develop.my.site.com/ESWPortfolioWebChat1777093773265/assets/js/bootstrap.min.js";
-
-      script.async = true;
-      script.setAttribute("data-salesforce", "true");
-
-      script.onload = initEmbeddedMessaging;
-
-      document.body.appendChild(script);
-    } else {
-      initEmbeddedMessaging();
-    }
+    document.body.appendChild(script);
   }, []);
 
   return null;
